4b3734b9db3ffd682b90da3d766a6a64a2f35e8b
FrostLeaf15 <85655364+FrostLeaf15@users.noreply.github.com>
2025-07-18 09:14:35 +0700

    New Update

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use App\Http\Controllers\PlayerController;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::get('/players', [PlayerController::class, 'index'])->name('players.index');
+Route::post('/players', [PlayerController::class, 'store'])->name('players.store');
+Route::post('/players/{player}/saldo', [PlayerController::class, 'updateSaldo'])->name('players.updateSaldo');
+Route::post('/players/{player}/property', [PlayerController::class, 'updateProperty'])->name('players.updateProperties');
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
